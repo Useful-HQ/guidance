f47407922ef7bb057eb8c857713ca7024d3b56ae
daniel@useful.app <daniel@useful.app>
2023-06-20 15:36:31 -0700

    Try pinning pyparser

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         "gptcache",
         "openai>=0.27",
         "anthropic",
-        "pyparsing",
+        "pyparsing>=3.1.0",
         "pygtrie",
         "platformdirs",
         "tiktoken>=0.3",
